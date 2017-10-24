ff016f62262edb0991d7dd3b085de469059b3bab
jasviers <jasviers@gmail.com>
2017-10-24 20:07:00 +0200

    Unas cosas iniciales bonitas

--- actualizar.py
+++ actualizar.py
@@ -1,1 +1,14 @@
-print("adios mundo")
+'''
+Función de actualizar
+-Entrada:
+	-Matriz de booleanos
+-Funcionalidad:
+	-Recibe una matriz de boleanos y devuelve otra
+ con los estados actualizados.
+-Salida:
+	-Nueva matriz
+'''
+import cesar
+
+def actualizar(matriz):
+	
