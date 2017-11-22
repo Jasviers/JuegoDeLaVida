2cb24d995c411d91bf17c1c1fc880f0d0b88c9cd
Alfedi <alfedi3867@hotmail.com>
2017-11-22 21:56:25 +0100

    Debería estar acabado. (No estoy del todo seguro)

--- actualizar.py
+++ actualizar.py
@@ -9,6 +9,12 @@ Función de actualizar
 	-Nueva matriz
 '''
 import cesar
+import copy
 
 def actualizar(matriz):
-	
+	mat_act = copy.deepcopy(matriz)
+	for i in range(len(matriz)):
+		j = 0
+		while j < len(matriz[0]):
+			 mat_act = cesar.cesar(i, j, matriz)
+	return mat_act
